7a8d42619e0a7a2247d21d531d343f8dbffaa3b5
Emperor <141957130+C0de-Emperor@users.noreply.github.com>
2024-11-24 21:37:47 +0100

    Update VI - Les Boucles While.py

--- VI - Les Boucles While.py
+++ VI - Les Boucles While.py
@@ -1,1 +1,16 @@
+############################################
+############# les Boucles While ############
+############################################
 
+"""
+Une alternative à l'instruction for couramment utilisée en informatique est la boucle while. 
+Avec ce type de boucle, 
+    une série d'instructions est exécutée tant qu'une condition est vraie. 
+"""
+
+# Par exemple :
+
+i = 1
+while i <= 4:
+    print(i)
+    i = i + 1
