b208f96d2b5c7135405ae6df1fd692fba88eae13
Emperor <141957130+C0de-Emperor@users.noreply.github.com>
2024-11-24 12:48:29 +0100

    Update 0-Introduction.py

--- 0-Introduction.py
+++ 0-Introduction.py
@@ -7,3 +7,21 @@
     Il se compose de 29 mots clés different qui peuvet tous etre arrangé de plusieurs manières differentes, 
     et de 20 fonctions de base.
 """
+
+# Dans les chapitres suivants j'utiliserais la fonction print()
+
+"""
+La finction print permet d'afficher des elements dans la console.
+
+ils seront de la forme : 
+print("1-" + str( variableInterresante )
+
+c'est pour vous aider a mieux vous reperer dans la console mais ne pretez pas attention au :
+"1-" + str( )
+"""
+
+"""
+Pour mieux travailler, vous pouvez telecharger les fichiers et les executer sur votre ordinateur
+
+pour cela il faut aller dans : Code -> Télécharger
+"""
