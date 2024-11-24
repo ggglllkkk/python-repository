da0ff2259119d2384ff6a800dbf08997b8586605
Emperor <141957130+C0de-Emperor@users.noreply.github.com>
2024-11-24 21:51:22 +0100

    Update VI - Les Boucles While.py

--- VI - Les Boucles While.py
+++ VI - Les Boucles While.py
@@ -31,3 +31,27 @@ Faites bien attention aux tests et à l'incrémentation que vous utilisez,
 Vous pouvez néanmoins toujours stopper l'exécution d'un script Python à l'aide de la combinaison de touches Ctrl-C
     (c'est-à-dire en pressant simultanément les touches Ctrl et C).
 """
+
+# La fonction input() :
+
+"""
+La fonction input() en Python permet de lire une entrée utilisateur depuis l'entrée standard (généralement le clavier).
+
+Elle prend en argument un message (sous la forme d'une chaîne de caractères), 
+    demande à l'utilisateur d'entrer une valeur et renvoie celle-ci sous forme d'une chaîne de caractères, 
+    qu'il faut ensuite convertir si besoin.
+"""
+
+entreeUtilisateur = input("saisir une lettre : ")
+
+print("2  : ", entreeUtilisateur )
+
+"""
+La boucle while combinée à la fonction input() peut s'avérer commode lorsqu'on souhaite demander à l'utilisateur une valeur numérique. 
+"""
+
+i = 0
+while i < 10:
+                            # Ici, le programme ne s'arretera pas tant que l'utilisateur n'aura pas saisi une valeur superieur a 10
+    reponse = input("Entrez un entier supérieur à 10 : ")
+    i = int(reponse)
