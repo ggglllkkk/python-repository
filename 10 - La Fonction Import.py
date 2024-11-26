e8dd4554a045d44b2bbe765b6cc31af819e8ac87
Emperor <141957130+C0de-Emperor@users.noreply.github.com>
2024-11-26 10:02:59 +0100

    Update 10 - La Fonction Import.py

--- 10 - La Fonction Import.py
+++ 10 - La Fonction Import.py
@@ -1,1 +1,23 @@
+############################################
+############ La Fonction import ############
+############################################
 
+# Définition : 
+
+"""
+Les modules sont des programmes Python qui contiennent des fonctions que l'on est amené à souvent réutiliser
+    (on les appelle aussi bibliothèques, ou libraries en anglais). 
+Ce sont des « boîtes à outils » qui vous seront très utiles.
+
+Les développeurs de Python ont mis au point de nombreux modules qui effectuent différentes tâches. 
+Pour cette raison, 
+    prenez toujours le réflexe de vérifier si une partie du code que vous souhaitez écrire n'existe pas déjà sous forme de module.
+
+La plupart de ces modules sont déjà installés dans les versions standards de Python. 
+Vous pouvez accéder à une documentation exhaustive sur le site de Python. 
+
+N'hésitez pas à explorer un peu ce site, 
+    la quantité de modules disponibles est impressionnante (plus de 300 modules).
+"""
+
+# Importation de modules :
