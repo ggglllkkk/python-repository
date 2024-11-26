c7a279be46a5f80b13649f8c85421e8c3a48ffd6
Emperor <141957130+C0de-Emperor@users.noreply.github.com>
2024-11-26 09:23:54 +0100

    Update 0 - Introduction.py

--- 0 - Introduction.py
+++ 0 - Introduction.py
@@ -33,3 +33,24 @@ pour cela il faut aller dans :
         Code -> "Cliquez sur le fichier qui vous interresse" -> "En haut à côté du mot : "Raw" vous avez un bouton "télécharger" "
         
 """
+
+"""
+⚠⚠⚠ Conventions ⚠⚠⚠
+
+Pour améliorer la lisibilité de votre script par autrui ou pour vous même, il est neccessaires de suivre certaines règles :
+
+1 - un nom de variable doit toujours être en anglais.
+2 - un nom de variable doit toujours être en rapport avec son rôle.
+3 - pour nommer un variable, ne mettez pas d'espace, la première lettre du premier mot doit être en minuscule, la première lettre des autres mots doit être en majuscule :
+
+exemple :
+    - le nom d'une variable qui stockerai la vitesse d'une balle serai : ballVelocity
+    - le nom d'une variable qui stockerai si un nombre est positif : isGreaterThanZero
+
+⚠ Attention ⚠
+
+
+    
+
+"""
+
